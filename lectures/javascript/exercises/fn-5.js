97f22f47a6a40000c655426779837f0c6c412421
Lee Seul <love.and.seul@gmail.com>
2025-07-01 16:25:23 +0900

    chore: Add exercise folder

--- lectures/javascript/exercises/fn-5.js
+++
@@ -1,4 +0,0 @@
-// fn-4의 getNextWeek 함수를 발전시켜
-// 다음과 같이 각 수업 별 요일을 선택하는
-// HTML을 작성하시오.(각각 요일 순회하는 순수함수로 작성)
-// pure function + callback function
--- lectures/typescript/dist/hello.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const myName = "Senior Coding";
-console.log(`Hello, ${myName}`);
-const myAge = 33;
-console.log("🚀 myAge:", myAge);
