c7d96577fc6d6310ccfde52e4dade7fb4ade42d5
Lee Seul <love.and.seul@gmail.com>
2025-07-25 18:32:18 +0900

    Exercise: Practice utility type

--- lectures/typescript/src/exercise/util-combine.ts
+++ lectures/typescript/src/exercise/util-combine.ts
@@ -11,10 +11,10 @@ interface IDept {
   captain: string;
 }
 
+//(T & U)[k] => T[k] or U[k] : 교집합이 없는 경우엔 해당 키에 해당 하는 타입을 가져온다
 type Combine<T, U> = {
   [k in keyof (T & U)]: k extends keyof T & keyof U ? T[k] | U[k] : (T & U)[k];
 };
-
 type ICombined = Combine<IUser, IDept>;
 
 let combineX: ICombined = {
@@ -24,6 +24,7 @@ let combineX: ICombined = {
   dname: "bbb",
   captain: "ccc",
 };
+
 let combineY: ICombined = {
   id: 0,
   age: "33세",
---
+++ lectures/typescript/src/exercise/util-record-users.ts
@@ -0,0 +1,16 @@
+type R = Record<string, number>;
+// type R = { [k:string]: number };
+
+// ex2) 다음 객체들을 하나로 합쳐(extend) 보세요. (id, name, age, addr)
+let users = [{ name: "Hong" }, { age: 23 }, { id: 1, addr: "Seoul" }];
+
+type Users = typeof users;
+type FullUser1 = Record<string, string | number>;
+type FullUser2 = Partial<Record<keyof Users[number], string | number>>;
+type FullUser = {
+  [k in keyof Users[number]]: Users[number][k];
+};
+const ret: FullUser = users.reduce((acc, user) => ({ ...acc, ...user }), {});
+// {}
+
+export {};
