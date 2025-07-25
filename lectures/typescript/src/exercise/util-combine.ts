cae7059ddb799ca784dfebc6df09042f58a535b5
Lee Seul <love.and.seul@gmail.com>
2025-07-25 15:54:45 +0900

    Exercise: Util-combine.ts

---
+++ lectures/typescript/src/exercise/util-combine.ts
@@ -0,0 +1,33 @@
+interface IUser {
+  id: number;
+  age: number;
+  name: string;
+}
+
+interface IDept {
+  id: number;
+  age: string;
+  dname: string;
+  captain: string;
+}
+
+type Combine<T, U> = {
+  [k in keyof (T & U)]: k extends keyof T & keyof U ? T[k] | U[k] : (T & U)[k];
+};
+
+type ICombined = Combine<IUser, IDept>;
+
+let combineX: ICombined = {
+  id: 0,
+  age: 33,
+  name: "aaa",
+  dname: "bbb",
+  captain: "ccc",
+};
+let combineY: ICombined = {
+  id: 0,
+  age: "33세",
+  name: "aaa",
+  dname: "bbb",
+  captain: "ccc",
+};
