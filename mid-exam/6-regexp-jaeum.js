5d132d21ea55dbe48262499a251611d2db809733
Lee Seul <love.and.seul@gmail.com>
2025-07-27 22:22:24 +0900

    Exercise: 6-regexep-jaeum

--- mid-exam/6-regexp-jaeum.js
+++ mid-exam/6-regexp-jaeum.js
@@ -1,19 +1,9 @@
-/*
-TODO: 강의영상 보고, 코드 정리
-- 인프런 강의
-- 룸 강의
-
-=> 내용 정리 안되는 경우 수업때 질문!
-
-
-
-*/
-
 import assert from "assert";
 
-const ALPHA_NUMERIC = [..."LMNRlmnr013678"].map((a) => a.charCodeAt());
+// 문자열이 한글 자음으로 끝나는지 체크하는 함수를 작성하시오.
+const ALPHA_NUMERIC = [..."LMNRlmnr136780"].map((a) => a.charCodeAt());
 // ["L", "M", "N", "R", "l", "m", "n", "r",...]
-// console.log("🚀 ~ ALPHA_NUMERIC: ", ALPHA_NUMERIC);
+
 const ㄱ = "ㄱ".charCodeAt();
 const ㅎ = "ㅎ".charCodeAt();
 const 가 = "가".charCodeAt();
@@ -23,23 +13,18 @@ const 힣 = "힣".charCodeAt();
 // }
 
 const isEndJaum = (str) => {
-  const lastChar = str.charCodeAt(str.length - 1); // 마지막글자 charcode
+  const lastChar = str.charCodeAt(str.length - 1);
 
   // 1. 단독 자음인지 검사
-  // if (/[ㄱ-ㅎ]/.test(lastChar)) return true;
   if (lastChar >= ㄱ && lastChar <= ㅎ) return true;
-
   // 2. 한글 완성형이고 받침이 있는지 확인
   if (lastChar >= 가 && lastChar <= 힣 && (lastChar - 가) % 28 !== 0)
     return true;
-
   // 3. 알파벳, 숫자 예외처리
   if (ALPHA_NUMERIC.includes(lastChar)) return true;
-
   return false;
 };
 
-// 문자열이 한글 자음으로 끝나는지 체크하는 함수를 작성하시오.
 isEndJaum("강원도"); // false
 isEndJaum("바라당"); // true
 isEndJaum("ㅜㅜ"); // false
@@ -62,33 +47,24 @@ assert.equal(isEndJaum("알파벳L"), true);
 //===================================================
 
 // 조사 '이/가, 을/를, 은/는'를 알아서 붙이는 함수를 작성하시오.
+
+const iga = (str) => {
+  return isEndJaum(str) ? "이" : "가";
+};
+const eunun = (str) => {
+  return isEndJaum(str) ? "은" : "는";
+};
+const eulul = (str) => {
+  return isEndJaum(str) ? "을" : "를";
+};
+const eyuya = (str) => {
+  return isEndJaum(str) ? "이어야" : "여야";
+};
+
 // `고성군${iga("고성군")}`; // 고성군이  cf. `강원도${iga('강원도')}` ⇒ 강원도가
 // `고성군${eunun("고성군")}`; // 고성군은  cf. `강원도${eunun('강원도')}` ⇒ 강원도는
 // `고성군${eulul("고성군")}`; // 고성군을  cf. `강원도${eulul('강원도')}` ⇒ 강원도를
 // (추가) ~이어야/여야, ~이랑/랑           isEndJaum('북면') ?  '이' : '가')
-
-// const iga = (str) => {
-//   if (isEndJaum(str)) return "이";
-//   return "가";
-// };
-// const eunun = (str) => {
-//   if (isEndJaum(str)) return "은";
-//   return "는";
-// };
-// const eulul = (str) => {
-//   if (isEndJaum(str)) return "을";
-//   return "를";
-// };
-// const eyuya = (str) => {
-//   if (isEndJaum(str)) return "이어야";
-//   return "여야";
-// };
-
-const josa = (str, jaum, moum) => (isEndJaum(str) ? jaum : moum);
-const iga = (str) => josa(str, "이", "가");
-const eunun = (str) => josa(str, "은", "는");
-const eulul = (str) => josa(str, "을", "를");
-const eyuya = (str) => josa(str, "이어야", "여야");
 
 assert.equal(`고성군${iga("고성군")}`, "고성군이");
 assert.equal(`고성군${eunun("고성군")}`, "고성군은");
